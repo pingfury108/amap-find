9514ba9e04e0e1526c62aaface33ab2eb72cf53a
pingfury <pingfury@outlook.com>
2024-05-06 02:35:14 +0800

    try message

--- content.ts
+++ content.ts
@@ -1,27 +1,30 @@
-import { sendToBackground } from '@plasmohq/messaging';
-import type { PlasmoCSConfig } from "plasmo";
-import u from 'umbrellajs';
+import type { PlasmoCSConfig } from "plasmo"
+import u from "umbrellajs"
+
+import { sendToBackground } from "@plasmohq/messaging"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://amap.com/*"]
 }
 
 window.addEventListener("load", () => {
-  console.log("你说神马");
+  console.log("你说神马")
   //console.log(u('#renderArrowLayer'));
   document.addEventListener("click", function (event) {
     // 在控制台输出点击事件的信息
-    console.log("鼠标点击位置：", event.clientX, event.clientY);
+    console.log("鼠标点击位置：", event.clientX, event.clientY)
     //console.log(u('#renderArrowLayer').html());
-    var placebox = u('.placebox');
-    console.log("place-id: ", placebox.attr("data-id"));
-    var place = u(".place-panel");
-    var name = place.find(".poiname").text();
-    var addr = place.find(".feedaddr").text();
-    var phone = place.find(".feedphone").text();
-    console.log("name: ", name);
-    console.log("addr: ", addr);
-    console.log("phone: ", phone);
-    sendToBackground('dataToDisplay', { message: 'Hello from content scripts!' + name + addr + phone });
-  });
+    var placebox = u("#amapBox")
+    console.log("place-id: ", placebox.attr("data-id"))
+    var place = u(".place-panel")
+    var name = place.find(".poiname").text()
+    var addr = place.find(".feedaddr").text()
+    var phone = place.find(".feedphone").text()
+    console.log("name: ", name)
+    console.log("addr: ", addr)
+    console.log("phone: ", phone)
+    //const placePort = usePort("place")
+    //placePort.send({ hello: "world" }
+    sendToBackground({ action: "place", data: name + addr + phone })
+  })
 })
--- sidepanel.tsx
+++ sidepanel.tsx
@@ -1,28 +1,27 @@
-import { relayMessage } from '@plasmohq/messaging';
-import { useState } from "react";
+import { useState } from "react"
 
+import { useMessage } from "@plasmohq/messaging/hook"
 
 function IndexSidePanel() {
-    const [data, setData] = useState("")
+  const [data, setData] = useState("")
+  useMessage<string, string>(async (req, res) => {
+    console.log("Received data in sidebar:", req.data)
+    // 更新侧边栏UI或执行其他操作
+    setData(req.data)
+    console.log(req)
+  })
 
-    relayMessage('dataToDisplay', (d) => {
-        const message = d.message;
-        console.log(message);
-        setData(message);
-    });
-    return (
-        <div
-            style={{
-                display: "flex",
-                flexDirection: "column",
-                padding: 16
-            }}>
-            这是侧边栏
-            <span>
-                {data}
-            </span>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        padding: 16
+      }}>
+      这是侧边栏
+      <span>{data}</span>
+    </div>
+  )
 }
 
 export default IndexSidePanel
