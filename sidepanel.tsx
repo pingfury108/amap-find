9c7e085eef10b68eb8b7340d6d7116a15433980d
pingfury <pingfury@outlook.com>
2024-05-06 23:48:38 +0800

    fix: try storage

--- sidepanel.tsx
+++ sidepanel.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-
 import { useMessage } from "@plasmohq/messaging/hook";
+import { Storage } from "@plasmohq/storage";
+import { useState } from "react";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import "~style.css";
 
+
+const storage = new Storage();
+const storage_key = "place-data";
 
 function PhoneItem({ phone }) {
   const handleCopy = () => {
@@ -24,9 +27,24 @@ function PhoneItem({ phone }) {
 }
 
 function IndexSidePanel() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState("");
 
   useMessage<string, string>(async (req, res) => {
+    var place_ss = await storage.get(storage_key);
+    console.log(place_ss, "ffff");
+    if (place_ss) {
+      //var place: Array<any> = JSON.parse(place_ss);
+      var place: Array<any> = place_ss;
+      place.push(req.data);
+      var place_2 = place.filter((item, index, self) =>
+        index === self.findIndex((t) => (
+          t.id === item.id
+        ))
+      );
+      storage.set(storage_key, place_2);
+    } else {
+      storage.set(storage_key, JSON.stringify([req.data,]));
+    }
     setData(req.data);
   })
 
@@ -47,10 +65,7 @@ function IndexSidePanel() {
     )
   } else {
     return (
-      <div style={{
-        padding: 16
-      }} className="text-base">这里没有电话数据哦</div>
-    )
+      <div style={{ padding: 16 }} className="text-base">这里没有电话数据哦</div>)
   }
 }
 
