577e7b132cb6c24866188d4fabb27c21d2f0f21c
qi.yongqiang <qi.yongqiang@dian-tong.com>
2024-05-06 15:42:42 +0800

    fix: fix content

--- content.ts
+++ content.ts
@@ -8,23 +8,23 @@ export const config: PlasmoCSConfig = {
 }
 
 window.addEventListener("load", () => {
-  console.log("你说神马")
-  //console.log(u('#renderArrowLayer'));
   document.addEventListener("click", function (event) {
+
     // 在控制台输出点击事件的信息
     console.log("鼠标点击位置：", event.clientX, event.clientY)
-    //console.log(u('#renderArrowLayer').html());
-    var placebox = u("#amapBox")
-    console.log("place-id: ", placebox.attr("data-id"))
-    var place = u(".place-panel")
-    var name = place.find(".poiname").text()
-    var addr = place.find(".feedaddr").text()
-    var phone = place.find(".feedphone").text()
-    console.log("name: ", name)
-    console.log("addr: ", addr)
-    console.log("phone: ", phone)
-    //const placePort = usePort("place")
-    //placePort.send({ hello: "world" }
-    sendToBackground({ action: "place", data: name + addr + phone })
+    setTimeout(() => {
+      var placebox = u(".placebox")
+      var place = u(".place-panel")
+      var data = {
+        id: placebox.attr("data-id"),
+        name: place.find(".poiname").text(),
+        addr: place.find(".feedaddr").text(),
+        phone: place.find(".feedphone").text(),
+      }
+      console.log("place: ", data)
+      sendToBackground({
+        action: "place", data: data
+      })
+    }, 1 * 1000);
   })
 })
--- sidepanel.tsx
+++ sidepanel.tsx
@@ -1,27 +1,50 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import { useMessage } from "@plasmohq/messaging/hook"
+import { useMessage } from "@plasmohq/messaging/hook";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+
+function PhoneItem({ phone }) {
+  const handleCopy = () => {
+  };
+  const phones = phone.trim().split(/\s+/);
+  console.log(phones);
+
+  if (phones.length > 0) {
+    return phones.map(p => <p>
+      <span>{p}</span>
+      <span></span>
+      <CopyToClipboard text={p}>
+        <button onClick={handleCopy}>点击复制电话</button>
+      </CopyToClipboard>
+    </p>);
+  } else {
+    return (<></>)
+  }
+}
 
 function IndexSidePanel() {
   const [data, setData] = useState("")
+
   useMessage<string, string>(async (req, res) => {
-    console.log("Received data in sidebar:", req.data)
-    // 更新侧边栏UI或执行其他操作
-    setData(req.data)
-    console.log(req)
+    setData(req.data);
   })
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      这是侧边栏
-      <span>{data}</span>
-    </div>
-  )
+  if (data.phone) {
+    return (
+      <div>
+        <div>
+          <span>{data.name}</span> |
+          <span>{data.addr}</span>
+        </div>
+        <PhoneItem phone={data.phone} />
+      </div>
+    )
+  } else {
+    return (
+      <div>这里没有电话数据哦</div>
+    )
+  }
 }
 
 export default IndexSidePanel
