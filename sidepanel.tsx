816e51e53ba493c580d8f7ee05726d384b10e2e1
qi.yongqiang <qi.yongqiang@dian-tong.com>
2024-05-08 16:57:57 +0800

    fix: sidepanel displace

--- sidepanel.tsx
+++ sidepanel.tsx
@@ -9,27 +9,73 @@ import "~style.css";
 const storage = new Storage();
 const storage_key = "place-data";
 
+class PlaceData {
+  id: String
+  name: String
+  addr: String
+  phone: String
+}
+
+function ParsePhone(phone: String) {
+  if (phone) {
+    return phone.replace(/\(/g, "").replace(/\)/g, "-").replace(/\;/g, " ").replace(/\,/g, " ").trim().split(/\s+/)
+  } else {
+    return []
+  }
+}
+
 function PhoneItem({ phone }) {
   const handleCopy = () => {
   };
-  const phones = phone.replace("(", "").replace(")", "-").replace(";", " ").replace(",", " ").trim().split(/\s+/);
+  var phones = ParsePhone(phone);
 
   if (phones.length > 0) {
-    return phones.map(p => <div key="phone" className="d-phone grid grid-cols-3">
-      <div className="text-lg phone col-span-2">{p}</div>
-      <div className="col-span-1 text-base">
-        <CopyToClipboard text={p}>
-          <button onClick={handleCopy} className="border rounded-full text-blue-500">点击复制电话</button>
-        </CopyToClipboard>
+    return phones.map((p: string, index: number) => (
+      <div key={index} className="d-phone grid grid-cols-10">
+        <div className="text-lg phone col-span-8">{p}</div>
+        <div className="col-span-2 col-start-9 col-end-11 text-base">
+          <CopyToClipboard text={p}>
+            <button onClick={handleCopy} className="border rounded-full text-blue-500 w-full">复制</button>
+          </CopyToClipboard>
+        </div>
       </div>
-    </div>);
+    ));
   } else {
     return (<></>)
   }
 }
 
+function History({ data }) {
+
+  const p = (s: String) => {
+    let pp = ParsePhone(s);
+    if (pp.length > 0) {
+      return pp[0]
+    } else {
+      return s
+    }
+  }
+
+  var data_l = []
+  if (data) {
+    data_l = data.filter(d => d.phone !== '')
+  }
+
+  return data_l.map((d, index: number) => (
+    <tr key={index}>
+      <td className="border">
+        <p className="truncate text-left">{d.name}</p>
+      </td>
+      <td className="border">
+        <p className="truncate text-left">{p(d.phone)}</p>
+      </td>
+    </tr>
+  ))
+}
+
 function IndexSidePanel() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState<PlaceData>(new (PlaceData));
+  const [storageData, setStorageData] = useState([]);
 
   useMessage<string, string>(async (req, res) => {
     var place_ss = await storage.get(storage_key);
@@ -43,41 +89,82 @@ function IndexSidePanel() {
           t.name === item.name
         ))
       );
-      storage.set(storage_key, place_2);
+      var place_3 = place_2.filter(p => p.phone !== '');
+      storage.set(storage_key, place_3);
+      setStorageData(place_3)
     } else {
-      storage.set(storage_key, JSON.stringify([req.data,]));
+      var place = [req.data,].filter(p => p.phone !== '');
+      storage.set(storage_key, place);
+      setStorageData(place);
     }
     setData(req.data);
   })
 
+  const copyTableToClipboard = () => {
+    // Convert table data to a formatted text
+    const textToCopy = storageData.map(row => [row.name, ...ParsePhone(row.phone)].join('\t')).join('\n');
+
+    // Copy the formatted text to clipboard
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        console.log('Table data copied to clipboard!');
+      })
+      .catch(err => {
+        console.error('Failed to copy: ', err);
+      });
+  };
+
+  var info = <div>这里没有电话数据哦</div>;
   if (data.phone) {
-    return (
-      <div className="container"
-        style={{
-          padding: 16
-        }}>
-        <div className="np text-base">
-          <div className="d-name grid grid-cols-3">
-            <div className="col-span-2">
-              {data.name}
-            </div>
-            <div className="col-span-1">
-              <CopyToClipboard text={data.name}>
-                <button className="border rounded-full text-blue-500">点击复制名称</button>
-              </CopyToClipboard>
-            </div>
+    info = (
+      <div>
+        <div className="grid grid-cols-10">
+          <div className="col-span-8">
+            {data.name}
           </div>
-          <div>{data.addr}</div>
+          <div className="col-span-2 col-start-9 col-end-11">
+            <CopyToClipboard text={data.name}>
+              <button className="border rounded-full text-blue-500 w-full">复制</button>
+            </CopyToClipboard>
+          </div>
         </div>
         <div className="container mt-5">
           <PhoneItem phone={data.phone} />
         </div>
       </div>
     )
-  } else {
-    return (
-      <div style={{ padding: 16 }} className="text-base">这里没有电话数据哦</div>)
   }
+
+  const clean_history = () => {
+    storage.clear();
+    setStorageData([]);
+  }
+
+  return (
+    <div className="container p-4 h-full text-base">
+      <div className="container text-base h-72">
+        {info}
+      </div>
+      <div className="border mt-10"></div>
+      <div className="container mt-5 pt-1">
+        <div className="grid grid-cols-2">
+          <button onClick={copyTableToClipboard} className="border rounded-full w-full">复制表格</button>
+          <button onClick={clean_history} className="border rounded-full w-full">清除历史</button>
+        </div>
+        <table className="mt-5 table-auto">
+          <thead>
+            <tr>
+              <th className="border">名称</th>
+              <th className="border">电话</th>
+            </tr>
+          </thead>
+          <tbody>
+            <History data={storageData} />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 
 export default IndexSidePanel
